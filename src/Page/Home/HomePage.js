8f7d0172bf3fbbb8314d9998d4dd5e61f56a5c59
Hossam1104 <hossammohamed1104@gmail.com>
2022-06-26 16:22:53 +0200

    finishing Home Page Design

--- src/Page/Home/HomePage.js
+++ src/Page/Home/HomePage.js
@@ -2,24 +2,37 @@ import React from "react";
 import NavBarLogin from "../../Components/Utility/NavBarLogin";
 import Slider from "../../Components/Home/Slider";
 import HomeCategory from "../../Components/Home/HomeCategory";
-import CardProductsComponents from "../../Components/Products/CardProductsComponents";
+import CardProductsContainer from "../../Components/Products/CardProductsContainer";
+import DiscountSection from "../../Components/Home/DiscountSection";
+import BrandFeatured from "../../Components/Brand/BrandFeatured";
+import Footer from "../../Components/Utility/Footer";
 
 const HomePage = () => {
   return (
     <div className="font" style={{ minHeight: "670px" }}>
       <NavBarLogin />
+
       <Slider />
+
       <HomeCategory />
-      <CardProductsComponents
+
+      <CardProductsContainer
         title="الاكثر مبيعا"
         btnTitle="المزيد"
         pathText="/products"
       />
-      <CardProductsComponents
+
+      <DiscountSection />
+
+      <CardProductsContainer
         title="احدث الازياء"
         btnTitle="المزيد"
         pathText="/products"
       />
+
+      <BrandFeatured title="اشهر الماركات" btnTitle="المزيد" />
+
+      <Footer />
     </div>
   );
 };
